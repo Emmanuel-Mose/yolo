eb7f8be6c254b3957b339d18a7944f97ff999ceb
Emmanuel-Mose <emmanuel.mose@student.moringaschool.com>
2023-03-13 09:14:45 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use('/api/products', productRoute)
 
 // Define the PORT
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5050
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)
